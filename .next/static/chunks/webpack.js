2aeafc1792194cbdc8dc7c07e85328a486ebc58c
peyoba <396217818@qq.com>
2025-04-07 06:44:41 +0800

    重新初始化仓库并提交代码

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "430f3a45b7d0a722"; }
+/******/ 		__webpack_require__.h = function() { return "bbcccb7184e5fd0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/admin/categories/add/page.tsx
+++ src/app/admin/categories/add/page.tsx
@@ -299,4 +299,7 @@ export default function AddCategoryPage() {
             </Button>
           </div>
         </form>
- 
+      </div>
+    </div>
+  );
+} 
